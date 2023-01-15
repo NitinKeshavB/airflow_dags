980b5adc794ab104d488ffd188cfc958f78eec41
NitinKeshavB <nitin.keshav11@gmail.com>
2023-01-15 23:34:56 +1100

    update schedule

--- dags/GPA/GPA_CAPFORCE_STAGING.py
+++ dags/GPA/GPA_CAPFORCE_STAGING.py
@@ -41,7 +41,7 @@ def _final_status(**kwargs):
 with DAG(
     dag_id="GPA_CAPFORCE_STAGING",
     start_date=pendulum.datetime(2023, 1, 14, tz="Australia/Sydney"),
-    schedule_interval="28 16 * * *",
+    schedule_interval="40 23 * * *",
     catchup=False,
 	render_template_as_native_obj=True,
     default_args={

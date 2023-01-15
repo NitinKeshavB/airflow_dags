49e007317924cd4ea0d49384775c9548cd447228
NitinKeshavB <nitin.keshav11@gmail.com>
2023-01-16 00:39:00 +1100

    upd

--- dags/GPA/GPA_CAPFORCE_LANDING.py
+++ dags/GPA/GPA_CAPFORCE_LANDING.py
@@ -41,7 +41,7 @@ def _final_status(**kwargs):
 with DAG(
     dag_id="GPA_CAPFORCE_LANDING",
     start_date=pendulum.datetime(2023, 1, 14, tz="Australia/Sydney"),
-    schedule_interval="38 0 * * *",
+    schedule_interval="40 0 * * *",
     catchup=False,
 	render_template_as_native_obj=True,
     default_args={
